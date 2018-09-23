a86543e4fc2e413d74cc6e37d4a88551e3bdfc0d
Diveyez <nemofbaltimore@gmail.com>
2018-09-23 00:46:38 -0700

    .

--- search.php
+++ search.php
@@ -37,7 +37,7 @@
 		echo "</br>"; // Gonna need more fancy shit here to wow them
 		$starttime = microtime(true); $endtime = microtime(true); printf("Search performed in %f seconds", $endtime - $starttime );
 	$ch = curl_init(); $encode = urlencode($query); $encode1 = urlencode($library); $encode2 = urlencode($server);
-	curl_setopt($ch, CURLOPT_URL, "http://".$encode2.":8983/solr/".$encode1."/select?q=".$encode."&fl=id&fl=score&wt=php&indent=true&&hl=on&debugQuery=on&rows=5000&hl.defaultSummary=true&hl.method=fastVector&hl.fl=".$encode."");
+	curl_setopt($ch, CURLOPT_URL, "http://".$encode2.":8983/solr/".$encode1."/select?q=".$encode."&fl=id&fl=score&wt=php&indent=true&hl=on&debugQuery=on&rows=5000&hl.snippets=1&hl.fragsize=0&hl.fl=".$encode."");
 	curl_setopt($ch, CURLOPT_HEADER, false); curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true); $re = curl_exec($ch); curl_close($ch); $arr;
 	eval("\$arr = " . $re . ";"); echo "<p><b>You searched for:</b><a></a><i>" . $query . "</i>"; echo "</br>";
 	echo "<b>Found:</b><i>" . $arr['response']['numFound'] . "</i> results\n </p>" ;
