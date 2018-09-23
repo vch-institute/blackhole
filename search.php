007ef7f6bbbefde91ff44b2c749ae15f125430e5
Diveyez <nemofbaltimore@gmail.com>
2018-09-23 00:54:15 -0700

    .

--- search.php
+++ search.php
@@ -31,14 +31,13 @@
                 <b>Server:</b><input name="server" type="text" />
                 <input class="button" name="submit" type="submit" value="Search The Galaxy"/></p></form></div>
 	<?php
-
 	if($_POST['search']) { echo "</br>Search Successful, here are the results."; }
 	else { echo ("</br>You did not search! Try again!"); }
 	$query = $_POST['search']; $library = $_POST['library']; $server = $_POST['server'];
 		echo "</br>"; // Gonna need more fancy shit here to wow them
 		$starttime = microtime(true); $endtime = microtime(true); printf("Search performed in %f seconds", $endtime - $starttime );
 	$ch = curl_init(); $encode = urlencode($query); $encode1 = urlencode($library); $encode2 = urlencode($server);
-	curl_setopt($ch, CURLOPT_URL, "http://".$encode2.":8983/solr/".$encode1."/select?q=".$encode."&fl=id&fl=score&wt=php&indent=true&debugQuery=true&rows=5000");
+	curl_setopt($ch, CURLOPT_URL, "http://".$encode2.":8983/solr/".$encode1."/select?q=".$encode."&fl=id&fl=score&wt=php&indent=true&&hl=on&debugQuery=on&rows=5000&hl.fl=".$encode."");
 	curl_setopt($ch, CURLOPT_HEADER, false); curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true); $re = curl_exec($ch); curl_close($ch); $arr;
 	eval("\$arr = " . $re . ";"); echo "<p><b>You searched for:</b><a></a><i>" . $query . "</i>"; echo "</br>";
 	echo "<b>Found:</b><i>" . $arr['response']['numFound'] . "</i> results\n </p>" ;
