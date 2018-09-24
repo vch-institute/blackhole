fde7aa752c724933f54375ca58be332bb931861e
Diveyez <nemofbaltimore@gmail.com>
2018-09-24 02:32:21 -0700

    SUPREME! CLEAN! HTML FILTER!

--- search.php
+++ search.php
@@ -42,6 +42,8 @@
 	//&fl=id&fl=score&fl=highlighting&fl=content&fl=manu&fl=creator&indent=true&hl=true&hl.fl=*&hl.snippets=1&hl.fragsize=0&hl.method=postings&wt=php&hl.highlightMultiTerm=true&hl.usePhraseHighlighter=true&hl.maxAnalyzedChars=1200&results=5000
 	// Clean
 	//&fl=id&fl=score&wt=php&indent=true&&hl=true&rows=5000&hl.fl=*&hl.snippets=1&hl.fragsize=0
+	// Supreme with html filter
+	//&fl=id&fl=score&fl=highlighting&fl=content&fl=manu&hl=true&&hl.fl=*&hl.snippets=1&hl.fragsize=0&hl.method=postings&wt=php&hl.highlightMultiTerm=true&hl.usePhraseHighlighter=true&hl.maxAnalyzedChars=1200&hl.encoder=html&results=5000
 	//testline
 	//http://localhost:8983/solr/Library/select?q=ak47&fl=id&fl=score&fl=*&ident=true&hl=true&explainOther=dog&hl.fl=*&hl.snippets=1&hl.fragsize=0&wt=php
 	curl_setopt($ch, CURLOPT_HEADER, false); curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true); $re = curl_exec($ch); curl_close($ch); $arr;
