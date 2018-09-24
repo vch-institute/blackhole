903995d5d56c2fa5137503c2fb734d30bc298b38
Diveyez <nemofbaltimore@gmail.com>
2018-09-24 02:28:58 -0700

    Improving Results with Excerts of the documents

--- backup-search.php
+++ backup-search.php
@@ -37,12 +37,7 @@
 		echo "</br>"; // Gonna need more fancy shit here to wow them
 		$starttime = microtime(true); $endtime = microtime(true); printf("Search performed in %f seconds", $endtime - $starttime );
 	$ch = curl_init(); $encode = urlencode($query); $encode1 = urlencode($library); $encode2 = urlencode($server);
-	curl_setopt($ch, CURLOPT_URL, "http://".$encode2.":8983/solr/".$encode1."/select?q=".$encode."&fl=id&fl=score&fl=highlighting&fl=content&fl=manu&fl=creator&indent=true&hl=true&hl.fl=*&hl.snippets=1&hl.fragsize=0&hl.method=postings&wt=php&hl.highlightMultiTerm=true&hl.usePhraseHighlighter=true&hl.maxAnalyzedChars=1200&results=5000");
-	// Supreme
-	//&fl=id&fl=score&fl=highlighting&fl=content&fl=manu&fl=creator&indent=true&hl=true&hl.fl=*&hl.snippets=1&hl.fragsize=0&hl.method=postings&wt=php&hl.highlightMultiTerm=true&hl.usePhraseHighlighter=true&hl.maxAnalyzedChars=1200&results=5000
-	// Clean
-	//&fl=id&fl=score&wt=php&indent=true&&hl=true&rows=5000&hl.fl=*&hl.snippets=1&hl.fragsize=0
-	//testline
+	curl_setopt($ch, CURLOPT_URL, "http://".$encode2.":8983/solr/".$encode1."/select?q=".$encode."&fl=id&fl=score&wt=php&indent=true&&hl=true&rows=5000&hl.fl=*&hl.snippets=1&hl.fragsize=0");
 	//http://localhost:8983/solr/Library/select?q=ak47&fl=id&fl=score&fl=*&ident=true&hl=true&explainOther=dog&hl.fl=*&hl.snippets=1&hl.fragsize=0&wt=php
 	curl_setopt($ch, CURLOPT_HEADER, false); curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true); $re = curl_exec($ch); curl_close($ch); $arr;
 	eval("\$arr = " . $re . ";"); echo "<p><b>You searched for:</b><a></a><i>" . $query . "</i>"; echo "</br>";
