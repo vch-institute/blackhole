53173e9c4d7559920252aa1f07bf0559e49a9006
Diveyez <nemofbaltimore@gmail.com>
2018-09-24 18:21:20 -0700

    .

--- index.php
+++ index.php
@@ -11,16 +11,16 @@
 			<div class="tooltip">
 			  		<span class="tooltiptext">Libraries: Contact System Administrator </br> Server: localhost</span>
 						<form method="post" action="search.php">
-            <p><b>Keywords:</b><input name="search" type="text" /><b>Library:</b><input name="library" type="text" /><b>Server:</b><input name="server" type="text" />
+            <searchbox><b>Keywords:</b><input name="search" type="text" /><b>Library:</b><input name="library" type="text" /><b>Server:</b><input name="server" type="text" /></searchbox>
             <input class="button1" name="submit" type="submit" value="Search The Galaxy"/></p></form>
 			</div>
-		</br><p>If you want the cheat sheet for searching with this tech, and any other search engine, checkout:</p> <a href="./queries.html"><p2>Commonly Uncommon Queries</a>.</p2>
-		</br><p> <a href="https://github.com/diveyez/blackhole/">Black Hole Search</a> by <a href="https://github.com/diveyez/">Diveyez</a></p>
-	</br><p>To learn more about and support the Black Hole Project, Click</p> <a href="./readme.html"><p2>HERE</a></p2>
-						<p>&copy; 2016-<?php echo date("Y"); ?></p>
-						</br><p>Built for <a href="https://universalcurrencysystems.com">Universal Currency Systems LLC</a>
-						</br>Los Angeles, California <a href="https://r2nhosting.com">R2N Hosting Solutions</a></br>
+		</br><pmain>If you want the cheat sheet for searching with this tech, and any other search engine, checkout:</p> <a href="./queries.html"><p2>Commonly Uncommon Queries</a>.</p2>
+		</br><pmain> <a href="https://github.com/diveyez/blackhole/">Black Hole Search</a> by <a href="https://github.com/diveyez/">Diveyez</a></p>
+	</br><pmain>To learn more about and support the Black Hole Project, Click <a href="./readme.html">HERE</a></pmain>
+						<pmain>&copy; 2016-<?php echo date("Y"); ?></pmain>
+					</br><pmain>Built for <a href="https://universalcurrencysystems.com">Universal Currency Systems LLC</a></pmain>
+						</br><pmain>Los Angeles, California <a href="https://r2nhosting.com">R2N Hosting Solutions</a></pmain></br>
 						<a href="http://lucene.apache.org/solr/"><img src="images/solr.png"/></img></a>
-						<p>Made with Apache Lucene Solr</p>
+						<pmain>Made with Apache Lucene Solr</pmain>
 						</center></body>
 </html>
