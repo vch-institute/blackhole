e56dff8afc984829fa08bafd470cbfba49176f58
Diveyez <nemofbaltimore@gmail.com>
2018-09-25 03:25:21 -0700

    MicroBuilds

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
-<html>
-<!-- ADD SECURITY HEADERS ASAP FOR PUBLIC LIBRARY -->
-			<head><meta charset="UTF-8"/>
-			<meta name="description" content="Black Hole Search"/>
-			<meta name="keywords" content="Bringing Solr Indexing and Machine Learning Interfacting to your Data Directories."/>
-			<meta name="author" content="diveyez@r2nhosting.com"/>
-			<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<html><head><meta charset="UTF-8"/>
+  		<meta name="description" content="Black Hole Search"/>
+  		<meta name="keywords" content="Bringing Solr Indexing and Machine Learning Interfacing to your Data Directories."/>
+  		<meta name="author" content="diveyez@r2nhosting.com"/>
+  		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 			<link rel="stylesheet" type="text/css" href="style.css"/>
-			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-			<link href="http://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"></head>
+      <link href="http://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      </head>
 <!-- ADD SECURITY HEADERS ASAP FOR PUBLIC LIBRARY -->
 <!-- TOP CONTENT -->
 			<body><div id="cont">
