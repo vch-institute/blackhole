3b965da94af2fd3987175fd107e767a0c057cfaa
Diveyez <nemofbaltimore@gmail.com>
2018-09-25 10:00:55 -0700

    .

--- news.php
+++ news.php
@@ -65,8 +65,8 @@
 							<li>[ ] Style, Decorate and polish a 1.0 final. With UX for users? (I hope) -(9-28-18)-</li></br>
 							<li>[ ] Web Browser Plugins for consumers.  -(9-28-18)-</li></br>
 							<li>[ ] Electron or equivalent application to remove need for browsers and webservers.  (Winter 2019 (Jan-April))</li></br>
-							<li>[ ] Solr enabled Jupyter Machine learning notebook to study data mining with computational capabilities.  (Winter 2019 (Jan-April))</li></br>
-							<li>[?] Software to use Ray Tracing (RTX) to enhance the machine learning ratio and overall functionality. (No Date; PLEASE @Nvidia, GIVE ME AN SDK)</li></br>
+							<li>[ ] Solr enabled Jupyter Machine learning notebook to study data mining with computational capabilities. (Jan-April))</li></br>
+							<li>[?] Software to use Ray Tracing (RTX) to enhance the machine learning ratio and overall functionality. (No Date)</li></br>
 							<h5><b>'x'</b> Indicates nearly full or full completion, periods <b>'.'</b> mark the in progress. </br>
 							<b>'?'</b> Indicates that there is no available information on completion date.</h5></br></div>
 <!--ROADMAP CHECKLIST -->
