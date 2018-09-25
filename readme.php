7193f4f8c8c4d2f800f0370c3460506e6068ef9a
Diveyez <nemofbaltimore@gmail.com>
2018-09-25 03:43:05 -0700

    </i>

--- readme.php
+++ readme.php
@@ -28,7 +28,7 @@
   <!-- Need to make the website a little more flashy -->
 <!-- Need to make the website a little more flashy -->
   <!-- Need to make the website a little more flashy -->
-  <h1><i>Black Hole Searching Platform</br></h1>
+  <h1><i>Black Hole Searching Platform</i>/br></h1>
    <h5>A multi-language www & machine learning capable Solr index searching solution that can fit in your pocket. </h5><h2><i>Eat The Sun!</i><h2>
       <h5>REQUIREMENTS,
             Linux or Windows Server with installed Webserver, GitHub, JRE8+, & Solr 7.5.0, 4+GB of RAM.</h5>
