5c3fc22b89a55344509f9d4bee438de93a0197fb
Diveyez <nemofbaltimore@gmail.com>
2018-09-24 18:45:31 -0700

    Menu time, <3 Java

--- index.php
+++ index.php
@@ -5,21 +5,25 @@
 			<meta name="author" content="diveyez@r2nhosting.com"/>
 			<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 			<link rel="stylesheet" type="text/css" href="style.css"/><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-		<link href="http://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"></head><body><center>
+		<link href="http://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"></head><body>
+<!-- MENU --><div class="navbar"><a href="./index.php">Home</a><a href="./news.php">News</a><div class="dropdown"><button class="dropbtn" onclick="myFunction()">Dropdown <i class="fa fa-caret-down"></i> </button>
+    <div class="dropdown-content" id="myDropdown"> <a href="#">Link 1</a> <a href="#">Link 2</a> <a href="#">Link 3</a> </div> </div><!-- MENU --> 
+		<center>
 						<div id="ww"><a href="./"><img src="./images/blackhole.png" height="250" width="350"></img></a></br>
-						<plight>Welcome to Black Hole, What do you want to search for?</plight></br>
-			<div class="tooltip">
-			  		<span class="tooltiptext">Libraries: <b>______________</b> </br> Server: localhost</span>
-						<form method="post" action="search.php">
+						<plight>Welcome to Black Hole, What do you want to search for?</plight></br><div class="tooltip">
+			  		<span class="tooltiptext">Libraries: <b>______________</b> </br> Server: localhost</span><form method="post" action="search.php">
             <searchbox><b>Keywords:</b><input name="search" type="text" /><b>Library:</b><input name="library" type="text" /><b>Server:</b><input name="server" type="text" /></searchbox>
-            <input class="button1" name="submit" type="submit" value="Search The Galaxy"/></p></form>
-			</div>
-		</br><pmain>If you want the cheat sheet for searching with this tech, and any other search engine, checkout:</p> <a href="./queries.html"><p2>Commonly Uncommon Queries</a>.</p2>
-	</br><pmain>To learn more about and support the Black Hole Project, Click <a href="./readme.html">HERE</a></pmain>
-					</br><pmain>Built for <a href="https://universalcurrencysystems.com">Universal Currency Systems LLC</a></pmain>
+            <input class="button1" name="submit" type="submit" value="Search The Galaxy"/></p></form></div>
+						</br><pmain>If you want the cheat sheet for searching with this tech, and any other search engine, checkout:</p> <a href="./queries.html"><p2>Commonly Uncommon Queries</a>.</p2>
+						</br><pmain>To learn more about and support the Black Hole Project, Click <a href="./readme.html">HERE</a></pmain>
+					  </br><pmain>Built for <a href="https://universalcurrencysystems.com">Universal Currency Systems LLC</a></pmain>
 						</br><pmain>Los Angeles, California <a href="https://r2nhosting.com">R2N Hosting Solutions</a></pmain></br>
-						<pmain>&copy; 2016-<?php echo date("Y"); ?></pmain><pmain> <a href="https://github.com/diveyez/blackhole/">Black Hole Search</a> by <a href="https://github.com/diveyez/">Diveyez</a></p>
+						<pmain>&copy; 2016-<?php echo date("Y"); ?></pmain>
+						<pmain> <a href="https://github.com/diveyez/blackhole/">Black Hole Search</a> by <a href="https://github.com/diveyez/">Diveyez</a></p>
 						<a href="http://lucene.apache.org/solr/"><img src="images/solr.png"/></img></a>
 						<pmain>Made with Apache Lucene Solr</pmain>
-					</center></div></body>
-</html>
+					  </center></div>
+<!-- MENU --><script>function myFunction() { document.getElementById("myDropdown").classList.toggle("show"); } window.onclick = function(e) { if (!e.target.matches('.dropbtn'))
+					{var myDropdown = document.getElementById("myDropdown"); if (myDropdown.classList.contains('show')) { myDropdown.classList.remove('show'); } } }
+					</script><!-- MENU -->
+				</body></html>
