9c9ec11f4ed6b318f3d9baac2ddc3dad2cd5d215
Diveyez <nemofbaltimore@gmail.com>
2018-09-24 18:12:30 -0700

    .

--- search.php
+++ search.php
@@ -4,7 +4,7 @@
   		<meta name="author" content="diveyez@r2nhosting.com"/>
   		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 			<link rel="stylesheet" type="text/css" href="style.css"/>
-    <link href="http://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"></head><body>
+      <link href="http://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"></head><body>
 			<a href="./"><img src="./images/blackhole.png" height="125" width=" 175"></img></a>
 								<div class="tooltip"><span class="tooltiptext"/>Library: "corename" </br> Server: localhost</span>
 								<form method="post" action="search.php"/>
@@ -22,7 +22,7 @@
 								echo "</br>"; // Gonna need more fancy shit here to wow them
 		 						$starttime = microtime(true); $endtime = microtime(true); printf("Search performed in %f seconds", $endtime - $starttime );
 								$ch = curl_init(); $encode = urlencode($query); $encode1 = urlencode($library); $encode2 = urlencode($server);
-								curl_setopt($ch,CURLOPT_URL,"http://".$encode2.":8983/solr/".$encode1."/select?q=".$encode."&fl=id&fl=score&fl=highlighting&fl=content&fl=manu&fl=creator&indent=true&hl=true&hl.q=".$encode."&hl.snippets=1&hl.fragsize=20&hl.method=unified&hl.bs.type=WORD&hl.defaultSummary=true&hl.maxAlternateFieldLength=25&hl.alternateField=content&hl.encoder=html&wt=php&hl.highlightMultiTerm=true&hl.fragmenter=regex&hl.regex.slop=0.1&hl.usePhraseHighlighter=true&hl.maxAnalyzedChars=10&results=50&hl.requireFieldMatch=true");curl_setopt($ch, CURLOPT_HEADER, false);
+								curl_setopt($ch,CURLOPT_URL,"http://".$encode2.":8983/solr/".$encode1."/select?q=".$encode."&fl=id&fl=score&fl=highlighting&fl=content&fl=manu&fl=creator&indent=true&hl=true&hl.q=".$encode."&hl.snippets=1&hl.fl=text_*&hl.fragsize=20&hl.method=unified&hl.bs.type=WORD&hl.defaultSummary=true&hl.maxAlternateFieldLength=25&hl.alternateField=content&hl.encoder=html&wt=php&hl.highlightMultiTerm=true&hl.tag.pre=<em>&hl.tag.post=</em>&hl.fragmenter=regex&hl.regex.slop=0.3&hl.usePhraseHighlighter=true&hl.maxAnalyzedChars=10&results=50&hl.requireFieldMatch=true");curl_setopt($ch, CURLOPT_HEADER, false);
 								curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true); $re = curl_exec($ch); curl_close($ch); $arr;
 								eval("\$arr = " . $re . ";"); echo "<p><b>You searched for:</b><i>" . $query . "</i>"; echo "</br>";
 								echo "<b>Found:</b><i>" . $arr['response']['numFound'] . "</i> results\n and displaying 50.</p>" ;
