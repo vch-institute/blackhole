a2ec595725393fa249dbf427136b02c67ee32c83
Diveyez <nemofbaltimore@gmail.com>
2018-09-25 08:38:38 -0700

    Searching...

---
+++ template/index.php
@@ -0,0 +1,15 @@
+<html><head><meta charset="UTF-8"/>
+  		<meta name="description" content="Black Hole Search"/>
+  		<meta name="keywords" content="Bringing Solr Indexing and Machine Learning Interfacing to your Data Directories."/>
+  		<meta name="author" content="diveyez@r2nhosting.com"/>
+  		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+			<link rel="stylesheet" type="text/css" href="style.css"/>
+      <link href="http://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <title>Black Hole Search - Notebooks, Libraries, AI, and Technology in one place.</title>
+      </head>
+<!-- ADD SECURITY HEADERS ASAP FOR PUBLIC LIBRARY -->
+<?php include("skel-menu.php"); ?>
+<?php include("skel-header-image.php"); ?>
+<?php include("skel-search.php"); ?>
+<body>
