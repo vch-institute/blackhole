d97b17f717aa8e9d64daf02b0bbe8180d8ce1e3f
Diveyez <nemofbaltimore@gmail.com>
2018-09-25 09:58:24 -0700

    foot

--- news.php
+++ news.php
@@ -71,11 +71,11 @@
 							<b>'?'</b> Indicates that there is no available information on completion date.</h5></br></div>
 <!--ROADMAP CHECKLIST -->
 
-<!-- FOOTER AREA -->
+<!-- FOOTER AREA --></div>
 						<div id="footer"><center>
 								<h5> <a href="https://github.com/diveyez/blackhole/">Black Hole Search</a> by <a href="https://github.com/diveyez/">Ricky 'Diveyez' N.</a></p><p>&copy; ® 2016-<?php echo date("Y"); ?></h5>
 												<h5>Los Angeles, California <a href="https://r2nhosting.com">R2N Hosting Solutions</a></h5> <h5>Made With</h5></br>
 												<a href="http://lucene.apache.org/solr/"><img src="images/solr.png"/></img></a>
 											</center></div>
 <!-- FOOTER AREA -->
-										</body></div></html>
+										</body></html>
