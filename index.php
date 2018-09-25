473e5474a802e85a1e6bcee05795d485b66a6cdb
Diveyez <nemofbaltimore@gmail.com>
2018-09-24 21:35:55 -0700

    ...

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
             <searchbox><b>Keywords:</b><input name="search" type="text" /><b>Library:</b><input name="library" type="text" /><b>Server:</b><input name="server" type="text" /></searchbox>
             <input class="button1" name="submit" type="submit" value="Search The Galaxy"/></p></form></div></br>
 						<pmain>If you want the cheat sheet for searching with this tech, and any other search engine, checkout:</p> <a href="./queries.php"><p2>Commonly Uncommon Queries</a>.</p2>
-						</br><pmain>To learn more about and support the Black Hole Project, Click <a href="./readme.html">HERE</a></pmain>
+						</br><pmain>To learn more about and support the Black Hole Project, Click <a href="./readme.php">HERE</a></pmain>
 					  </br><pmain>Built for <a href="https://universalcurrencysystems.com">Universal Currency Systems LLC</a></pmain>
 						</br><pmain>Los Angeles, California <a href="https://r2nhosting.com">R2N Hosting Solutions</a></pmain></br>
 						<pmain>&copy; 2016-<?php echo date("Y"); ?></pmain>
