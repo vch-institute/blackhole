680337362128c5ec8416b8c17374a9557655c180
Diveyez <nemofbaltimore@gmail.com>
2018-09-25 04:37:45 -0700

    .

--- check.php
+++ check.php
@@ -1,2 +1,2 @@
-<html><head></head><body></body>
-<?php include("./template/skel-footer.php"); ?>
+
+<?php include("./template/skel-header.php"); ?><?php include("./template/skel-menu.php"); ?><?php include("./template/skel-roadmap.php"); ?><?php include("./template/skel-footer.php"); ?>
