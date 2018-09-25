39eab42af705d06259be9f73a42f5ce3bc27ffe0
Diveyez <nemofbaltimore@gmail.com>
2018-09-24 23:57:15 -0700

    Many Updates

--- menu.php
+++ menu.php
@@ -19,3 +19,10 @@
   <li><a href="readme.php">Read Me</a></li>
   <li><a href="queries.php">HowTo: Queries Explained</a></li>
 </ul>
+
+<!-- FOOTER AREA -->
+						<div id="footer"><center>
+								<h5> <a href="https://github.com/diveyez/blackhole/">Black Hole Search</a> by <a href="https://github.com/diveyez/">Ricky 'Diveyez' N.</a></p><p>&copy; ® 2016-<?php echo date("Y"); ?></h5>
+												<h5>Los Angeles, California <a href="https://r2nhosting.com">R2N Hosting Solutions</a></h5> <h5>Made With</h5></br>
+												<a href="http://lucene.apache.org/solr/"><img src="images/solr.png"/></img></a>
+											</center></div></body></html>
---
+++ template/skel-searchcheats.php
@@ -0,0 +1,9 @@
+<!-- SEARCH CHEAT SHEET =P -->
+      <div id="cheats">
+      <b> The following is a sort of, cheat sheet for search queries.</b> <b> View the <a href="queries.php">Queries Explained</a> page for explainations.</b>
+      <i>"phraseyouwish"</i> <i>"adam +laverne"</i> <i>"bitcoin $6000..$80000"</i></br> <i>"'MICHAEL'*'DOUGLAS'"</i> <i>"'-MICHAEL'*'-DOUGLAS'"</i></br>
+      <i>"Something I Need To Find&hl.q=Something More Specific From That Document, Site, Etc. -*"</i></br></br><i>"Website.com:phrase/sentence" "booktitle:phrase/sentence"</i></br>
+      <i>"website1.com site:website2.com"</i><i>"Medicine book:'Antibiotics'-*"</i></br><i>"DAY:DATE Monday:February 1, MONTH:YEAR '1790'&hl.q=sumreme court&-*"</i></br>
+      <i>"Alcohol vs. Wine"</i><i>"DEFINE: 'whats on your mind'".</i></br><i>"How To book:'DEFINE: Home Made Solar Panels'-*"</i></br>
+      <i>"DEFINE:DAY:DATE Monday:February 1, MONTH:YEAR '1790'&hl.q=sumreme court&-*"</i></br></div>
+<!-- SEARCH CHEAT SHEET =P -->
