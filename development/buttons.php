d4eaeb3536a5a62b5fbb3dc715197b853aa8b564
Diveyez <nemofbaltimore@gmail.com>
2018-09-25 08:58:31 -0700

    css

--- development/buttons.php
+++ development/buttons.php
@@ -1,17 +1,9 @@
 <html>
-<!-- ADD SECURITY HEADERS ASAP FOR PUBLIC LIBRARY -->
-			<head><meta charset="UTF-8"/>
-			<meta name="description" content="Black Hole Search"/>
-			<meta name="keywords" content="Bringing Solr Indexing and Machine Learning Interfacting to your Data Directories."/>
-			<meta name="author" content="diveyez@r2nhosting.com"/>
-			<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-			<link rel="stylesheet" type="text/css" href="style.css"/>
-			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-			<link href="http://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"></head>
+
 <!-- ADD SECURITY HEADERS ASAP FOR PUBLIC LIBRARY -->
       <body>
 <!-- GO BACK button -->
-      <button onclick="goBack()">Go Back To Search</button>
+      <button id="back" onclick="goBack()">Go Back To Search</button>
       <script> function goBack() { window.history.back(); } </script>
 <!-- GO BACK button -->
 
@@ -27,8 +19,3 @@
       <button onclick="goBack()">Go Back To Main Page</button>
       <script> function goBack() { window.history.back(); } </script></br>
 <!-- back button -->
-<!-- FOOTER REGION-->
-      <div id="footer"><center>
-          <h5> <a href="https://github.com/diveyez/blackhole/">Black Hole Search</a> by <a href="https://github.com/diveyez/">Diveyez</a></p><p>&copy; 2016-<?php echo date("Y"); ?></h5>
-          <h5>Los Angeles, California <a href="https://r2nhosting.com">R2N Hosting Solutions</a></h5> <h5>Made With</h5></br>
-          <a href="http://lucene.apache.org/solr/"><img src="images/solr.png"/></img></a></center></div></div></body></html>
