974d1ce048528cff45c0515bdc953538a12fdf58
Diveyez <nemofbaltimore@gmail.com>
2018-09-25 16:53:00 -0700

    .

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
 <div id="background"></div>
 
-
+<div id="box"></div>
 			<body><div id="cont">
 
 
@@ -65,7 +65,7 @@
 <!-- MAIN CONTENT (SEARCH)-->
 
 			  		<div class="tooltip">
-						<span class="tooltiptext">Libraries:<i>"This is whatever you called the core during setup. ie: 'library' "</b> </br> Server: localhost</span><form method="post" action="search.php">
+						<span class="tooltiptext">Libraries:"This is whatever you called the core during setup. ie: 'library' "</b> </br> Server: localhost</span><form method="post" action="search.php">
             <searchbox><b>Keywords:</b><input name="search" type="text" /><b>Library:</b><input name="library" type="text" /><b>Server:</b><input name="server" type="text" /></searchbox>
             <input class="button1" name="submit" type="submit" value="Search The Galaxy"/></p></form></div></br>
 						<pmain>If you want the cheat sheet for searching with this tech, and any other search engine, checkout:</p> <a href="./queries.php"><p2>Commonly Uncommon Queries</a>.</p2>
