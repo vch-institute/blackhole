90f3641e5ecceb316873bb152b082f0b3c21a5a1
Diveyez <nemofbaltimore@gmail.com>
2018-09-25 05:00:41 -0700

    :+1:

--- development/check.php
+++ development/check.php
@@ -4,7 +4,7 @@ $ch = curl_init(); $encode = urlencode($query); $encode1 = urlencode($library); 
 curl_setopt($ch,CURLOPT_URL,"http://localhost:8983/solr/library/select?q=dryrun&fl=id&fl=score&fl=highlighting&fl=content&fl=manu&indent=true&hl=true&hl.q=dryrun&hl.snippets=1&hl.method=unified&hl.bs.type=SENTENCE&hl.maxAlternateFieldLength=5&hl.encoder=html&wt=php&hl.usePhraseHighlighter=true&hl.maxAnalyzedChars=15&results=50&hl.requireFieldMatch=true");
 curl_setopt($ch, CURLOPT_HEADER, false);
 curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true); $re = curl_exec($ch); curl_close($ch); $arr;
-eval("\$arr = " . $re . ";"); echo "" . $query . ""; ;
+eval("\$arr = " . $re . ;); echo "" . $query . ""; ;
 echo "" . $arr['response']['numFound'] . "\n" ;
 								echo "";
 								foreach($arr['response']['docs'] as $text)
