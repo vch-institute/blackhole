c344c6326eebe52d1e94060fded386dd7ca462bb
Diveyez <nemofbaltimore@gmail.com>
2018-09-25 04:59:50 -0700

    check.php js conversion

--- development/check.php
+++ development/check.php
@@ -1,11 +1,11 @@
 $query = $_POST['search']; $library = $_POST['library']; $server = $_POST['server'];
 $starttime = microtime(true); $endtime = microtime(true); printf("Search performed in %f seconds", $endtime - $starttime );
 $ch = curl_init(); $encode = urlencode($query); $encode1 = urlencode($library); $encode2 = urlencode($server);
-curl_setopt($ch,CURLOPT_URL,"http://".$encode2.":8983/solr/".$encode1."/select?q=".$encode."&fl=id&fl=score&fl=highlighting&fl=content&fl=manu&indent=true&hl=true&hl.q=".$encode."&hl.snippets=1&hl.method=unified&hl.bs.type=SENTENCE&hl.maxAlternateFieldLength=5&hl.encoder=html&wt=php&hl.tag.pre=<em>&hl.tag.post=</em>&hl.usePhraseHighlighter=true&hl.maxAnalyzedChars=15&results=50&hl.requireFieldMatch=true");
+curl_setopt($ch,CURLOPT_URL,"http://localhost:8983/solr/library/select?q=dryrun&fl=id&fl=score&fl=highlighting&fl=content&fl=manu&indent=true&hl=true&hl.q=dryrun&hl.snippets=1&hl.method=unified&hl.bs.type=SENTENCE&hl.maxAlternateFieldLength=5&hl.encoder=html&wt=php&hl.usePhraseHighlighter=true&hl.maxAnalyzedChars=15&results=50&hl.requireFieldMatch=true");
 curl_setopt($ch, CURLOPT_HEADER, false);
 curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true); $re = curl_exec($ch); curl_close($ch); $arr;
 eval("\$arr = " . $re . ";"); echo "" . $query . ""; ;
-echo "Found:" . $arr['response']['numFound'] . "\n" ;
+echo "" . $arr['response']['numFound'] . "\n" ;
 								echo "";
 								foreach($arr['response']['docs'] as $text)
 								{ echo "Found:" . $text['id'] . "" .$text['content'] . "" . "\n"; }
