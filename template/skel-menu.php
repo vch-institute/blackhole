1d2d10758db5e2789e05154608e0271251b43178
Diveyez <nemofbaltimore@gmail.com>
2018-09-25 00:17:35 -0700

    Skel

---
+++ template/skel-menu.php
@@ -0,0 +1,10 @@
+<!-- NAVIGATION MENU -->
+<menu>
+<ul id="buttons">
+  <li><a href="index.php">Home</a></li>
+  <li><a href="news.php">News</a></li>
+  <li><a href="readme.php">Read Me</a></li>
+  <li><a href="queries.php">Queries Explained</a></li>
+</ul>
+</menu>
+<!-- NAVIGATION MENU -->
