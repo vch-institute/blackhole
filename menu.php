c0612d4a82c038b6c7ee710237f411e43fbe0131
Diveyez <nemofbaltimore@gmail.com>
2018-09-24 23:58:55 -0700

    menu building

--- menu.php
+++ menu.php
@@ -9,16 +9,21 @@
 			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 			<link href="http://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"></head>
 <!-- ADD SECURITY HEADERS ASAP FOR PUBLIC LIBRARY -->
-<!-- TOP CONTENT -->
-			<body><div id="cont"><center>
-						<a href="./"><img src="images/blackhole.png" height="350" width="500"></img></a></br>
-<!-- TOP CONTENT -->
+
+<menu>
 <ul>
   <li><a href="index.php">Home</a></li>
   <li><a href="news.php">News</a></li>
   <li><a href="readme.php">Read Me</a></li>
   <li><a href="queries.php">HowTo: Queries Explained</a></li>
 </ul>
+</menu>
+
+<!-- TOP CONTENT -->
+			<body><div id="cont"><center>
+						<a href="./"><img src="images/blackhole.png" height="350" width="500"></img></a></br></div>
+<!-- TOP CONTENT -->
+
 
 <!-- FOOTER AREA -->
 						<div id="footer"><center>
