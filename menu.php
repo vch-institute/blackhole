bb91e2ea440281fb03e8037b45aac69db0cfbe2b
Diveyez <nemofbaltimore@gmail.com>
2018-09-25 00:15:25 -0700

    css

--- menu.php
+++ menu.php
@@ -15,7 +15,7 @@
   <li><a href="index.php">Home</a></li>
   <li><a href="news.php">News</a></li>
   <li><a href="readme.php">Read Me</a></li>
-  <li><a href="queries.php">HowTo: Queries Explained</a></li>
+  <li><a href="queries.php">Queries Explained</a></li>
 </ul>
 </menu>
 
