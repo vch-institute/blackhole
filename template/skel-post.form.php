14b2791584ee457093e0312e5c5ddccd2000018e
Diveyez <nemofbaltimore@gmail.com>
2018-09-25 12:33:52 -0700

    .

---
+++ template/skel-post.form.php
@@ -0,0 +1,9 @@
+<!-- MAIN CONTENT (SEARCH TOOLTIPS)-->
+<div class="tooltip">
+<span class="tooltiptext">Library:<i>'library'</b> </br> Server: localhost</span>
+								<form method="post" action="search.php"/>
+                <p><b>Keywords:</b><input name="search" type="text" />
+                <b>Library:</b><input name="library" type="text" />
+                <b>Server:</b><input name="server" type="text" />
+                <input class="button1" name="submit" type="submit" value="Search The Galaxy"/></p></form></div>
+<!-- MAIN CONTENT (SEARCH ARRAY CODE)-->
