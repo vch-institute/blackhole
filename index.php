421e3f1ee1a701bca4f03d8ae2546bfb1ac9f186
Diveyez <nemofbaltimore@gmail.com>
2018-09-24 23:54:39 -0700

    -i +p

--- index.php
+++ index.php
@@ -16,18 +16,18 @@
 <!-- TOP CONTENT -->
 <!-- MAIN CONTENT (SEARCH)-->
 			  		<div class="tooltip">
-						<span class="tooltiptext">Libraries:<i>"This is whatever you called the core during setup. I have many. ie: 'library' "</b> </br> Server: localhost</span><form method="post" action="search.php">
+						<span class="tooltiptext">Libraries:<p>"This is whatever you called the core during setup. I have many. ie: 'library' "</b> </br> Server: localhost</span><form method="post" action="search.php">
             <searchbox><b>Keywords:</b><input name="search" type="text" /><b>Library:</b><input name="library" type="text" /><b>Server:</b><input name="server" type="text" /></searchbox>
             <input class="button1" name="submit" type="submit" value="Search The Galaxy"/></p></form></div></br>
 						</br>
 						<!-- SEARCH CHEAT SHEET =P -->
 						      <div id="cheats">
 						      <b> The following is a sort of, cheat sheet for search queries.</b> <b> View the <a href="queries.php">Queries Explained</a> page for explainations.</b></br>
-						      <i>"phraseyouwish"</i> <i>"adam +laverne"</i> <i>"bitcoin $6000..$80000"</i> <i>"'MICHAEL'*'DOUGLAS'"</i> <i>"'-MICHAEL'*'-DOUGLAS'"</i></br>
-						      <i>"Something I Need To Find&hl.q=Something More Specific From That Document, Site, Etc. -*"</i><i>"Website.com:phrase/sentence" "booktitle:phrase/sentence"</i></br>
-						      <i>"website1.com site:website2.com"</i><i>"Medicine book:'Antibiotics'-*"</i><i>"DAY:DATE Monday:February 1, MONTH:YEAR '1790'&hl.q=sumreme court&-*"</i></br>
-						      <i>"Alcohol vs. Wine"</i><i>"DEFINE: 'whats on your mind'".</i><i>"How To book:'DEFINE: Home Made Solar Panels'-*"</i></br>
-						      <i>"DEFINE:DAY:DATE Monday:February 1, MONTH:YEAR '1790'&hl.q=sumreme court&-*"</i></br></div>
+						      <p>"phraseyouwish"</p> <p>"adam +laverne"</p> <p>"bitcoin $6000..$80000"</p> <p>"'MICHAEL'*'DOUGLAS'"</p> <p>"'-MICHAEL'*'-DOUGLAS'"</p></br>
+						      <p>"Something I Need To Find&hl.q=Something More Specific From That Document, Site, Etc. -*"</p><p>"Website.com:phrase/sentence" "booktitle:phrase/sentence"</p></br>
+						      <p>"website1.com site:website2.com"</p><p>"Medicine book:'Antibiotics'-*"</p><p>"DAY:DATE Monday:February 1, MONTH:YEAR '1790'&hl.q=sumreme court&-*"</p></br>
+						      <p>"Alcohol vs. Wine"</p><p>"DEFINE: 'whats on your mind'".</p><p>"How To book:'DEFINE: Home Made Solar Panels'-*"</p></br>
+						      <p>"DEFINE:DAY:DATE Monday:February 1, MONTH:YEAR '1790'&hl.q=sumreme court&-*"</p></br></div>
 						<!-- SEARCH CHEAT SHEET =P -->
 						<pmain>If you want the cheat sheet for searching with this tech, and any other search engine, checkout:</p> <a href="./queries.php"><p2>Commonly Uncommon Queries</a>.</p2>
 						</br><pmain>To learn more about and support the Black Hole Project, Click <a href="./readme.php">HERE</a></pmain>
