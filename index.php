f82d97200a720f5f0f67d465f1a70dc0b996aec5
Diveyez <nemofbaltimore@gmail.com>
2018-09-25 09:24:31 -0700

    nav

--- index.php
+++ index.php
@@ -16,18 +16,37 @@
 			<body><div id="cont">
 
 <!-- NAVIGATION MENU -->
+<!-- RESPONSIVE NAV BAR -->
+<div class="topnav" id="myTopnav">
+  <a href="index.php" class="active">Home</a>
+  <a href="about.php">About</a>
+  <a href="https://github.com/diveyez/blackhole">Contact Developer</a>
+  <div class="dropdown">
+    <button class="dropbtn">Explore
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="news.php">News</a>
+      <a href="queries.php">Queries Explained</a>
+      <a href="search.php">Search</a>
+    </div>
+  </div>
+  <a href="#about">About</a>
+  <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
+</div>
+<script>
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+</script>
 
-				<menu>
-				<ul id="buttons">
-				  <li><a href="index.php">Home</a></li>
-				  <li><a href="news.php">News</a></li>
-				  <li><a href="about.php">About</a></li>
-				  <li><a href="queries.php">Queries Explained</a></li>
-					<li><a href="https://github.com/diveyez/blackhole">Contact Developer</a></li>
-				</ul>
-				</menu>
+<!--RESPONSIVE NAV BAR -->
 
-<!-- NAVIGATION MENU -->
 
 <!-- TOP CONTENT -->
 
