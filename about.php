8e9788e7e7f04b5be9b81de9f01b524c630e2852
Diveyez <nemofbaltimore@gmail.com>
2018-09-25 09:31:54 -0700

    ...

--- about.php
+++ about.php
@@ -11,17 +11,36 @@
 <!-- ADD SECURITY HEADERS ASAP FOR PUBLIC LIBRARY -->
 
 <!-- TOP CONTENT -->
-        <body><center><div id="cont"><!-- NAVIGATION MENU -->
-				<menu>
-				<ul id="buttons">
-				  <li><a href="index.php">Home</a></li>
-				  <li><a href="news.php">News</a></li>
-				  <li><a href="about.php">About</a></li>
-				  <li><a href="queries.php">Queries Explained</a></li>
-					<li><a href="https://github.com/diveyez/blackhole">Contact Developer</a></li>
-				</ul>
-				</menu>
-				<!-- NAVIGATION MENU -->
+        <body><center><div id="cont"><!-- RESPONSIVE NAV BAR -->
+        <div class="topnav" id="myTopnav">
+          <a href="index.php" class="active">Home</a>
+          <a href="about.php">About</a>
+          <a href="https://github.com/diveyez/blackhole">Contact Developer</a>
+          <div class="dropdown">
+            <button class="dropbtn">Explore
+              <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+              <a href="news.php">News</a>
+              <a href="queries.php">Queries Explained</a>
+              <a href="search.php">Search</a>
+            </div>
+          </div>
+          <a href="#about">About</a>
+          <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
+        </div>
+        <script>
+        function myFunction() {
+            var x = document.getElementById("myTopnav");
+            if (x.className === "topnav") {
+                x.className += " responsive";
+            } else {
+                x.className = "topnav";
+            }
+        }
+        </script>
+
+        <!--RESPONSIVE NAV BAR -->
               <a href="./index.php"><img src="images/blackhole.png" height="350" width="500"></img></a></br>
 <!-- ABOUT CONTENT -->
 <div id="ww1">
