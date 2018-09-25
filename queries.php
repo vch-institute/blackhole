b42e929404274bc3f244daea328d0fb09bc2c9ab
Diveyez <nemofbaltimore@gmail.com>
2018-09-25 03:23:31 -0700

    Fixing a Typo

--- queries.php
+++ queries.php
@@ -1,6 +1,6 @@
 <html><head><meta charset="UTF-8"/>
   		<meta name="description" content="Black Hole Search"/>
-  		<meta name="keywords" content="Bringing Solr Indexing and Machine Learning Interfacting to your Data Directories."/>
+  		<meta name="keywords" content="Bringing Solr Indexing and Machine Learning Interfacing to your Data Directories."/>
   		<meta name="author" content="diveyez@r2nhosting.com"/>
   		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 			<link rel="stylesheet" type="text/css" href="style.css"/>
