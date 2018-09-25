8bc5f2271583eed30c1885a3c45fd496ef7ce3e5
Diveyez <nemofbaltimore@gmail.com>
2018-09-24 22:44:33 -0700

    ............

---
+++ development/roadmap.php
@@ -0,0 +1,17 @@
+<!-- ROADMAP CHECKLIST -->
+  <h3><b>Planned Features:</b></h3></br>
+      - [x] Test Core</br>
+      - [x] Full text output in results</br>
+      - [x] JSON/JavaScript version will release in the next 14 days</br>
+      - [x] Custom Schema's for the project</br>
+      - [x] Add more search fields (Continuing This Eternally)</br>
+      - [x] Instructions for Linux Server Install/Setup/Operation</br>
+      - [ ] Instructions for Windows Server Install/Setup/Operation</br>
+      - [ ] Incorporate third party software and plugins to improve functionality</br>
+      - [ ] Dockerize</br>
+      - [ ] Public Library for testing</br>
+      - [ ] Style and Decorate and publish a 1.0 final</br>
+      - [ ] Web Browser Plugins for consumers</br>
+      - [ ] Electron or equivalent application to remove need for browers and webservers</br>
+      - [ ] Machine learning notebook to study datamining with computational capabilities</br></br>
+<!-- ROADMAP CHECKLIST -->
