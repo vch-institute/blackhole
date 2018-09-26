33b8f955ac91699e4db9a88331099972938f5e0d
Diveyez <nemofbaltimore@gmail.com>
2018-09-25 18:22:03 -0700

    ..

--- index.php
+++ index.php
@@ -15,16 +15,16 @@
 <!-- LEFT -->
 <body><div id="boxleft">
   <div class="row">
-    <div class="column">TESTING-1left mi fa so la ti da</div>
     <div class="column">TESTING</div>
+    <div class="column">IN PROGRESS</div>
   </div></div>
 
 
 <!-- RIGHT -->
   <div id="boxright">
     <div class="row">
-      <div class="column">TESTING-1right do re mi fa so la ti do</div>
       <div class="column">TESTING</div>
+      <div class="column">IN PROGRESS</div>
     </div></div>
 
 			<body><div id="cont">
