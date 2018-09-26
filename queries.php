c82ed44ae822df39ecc2e1f14ea6b0cdbcff1eb6
Diveyez <nemofbaltimore@gmail.com>
2018-09-25 20:41:25 -0700

    .

--- queries.php
+++ queries.php
@@ -45,15 +45,14 @@
 			<a href="./"><center><img src="images/blackhole.png" height="125" width="190"></img></a></br></center>
 <!-- TOP CONTENT -->
 
-
+<center>
                 <div class="tooltip">
     						<span class="tooltiptext">Libraries:"This is whatever you called the core during setup. ie: 'library' "</b> </br> Server: localhost</span>
               <form method="post" action="search.php">
               <searchbox><b>Keywords:</b><input name="search" type="text" /><b>Library:</b>
               <input name="library" type="text" /><b>Server:</b><input name="server" type="text" /></searchbox>
               <input class="button1" name="submit" type="submit" value="Search The Galaxy"/></p></form></div></br>
-
-
+</center>
 <!-- CONTENT REGION -->
               <div id="pqueries">
               <h3>This is the cheat sheet to utilizing my technology.</h3>
