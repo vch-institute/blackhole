53083ff693fb36c44caf3e2f853db10a6875f6cf
Diveyez <nemofbaltimore@gmail.com>
2018-09-25 18:46:51 -0700

    .

--- about.php
+++ about.php
@@ -90,11 +90,11 @@
  </br></br>
 
 <img src="images/bookchair.jpg"></img>
-</br></center></div>
-
- <!-- FOOTER AREA -->
+</br></center> <!-- FOOTER AREA -->
              <div id="footer"><center>
                  <h5> <a href="https://github.com/diveyez/blackhole/">Black Hole Search</a> by <a href="https://github.com/diveyez/">Ricky 'Diveyez' N.</a></p><p>&copy; ® 2016-<?php echo date("Y"); ?></h5>
                          <h5>Los Angeles, California <a href="https://r2nhosting.com">R2N Hosting Solutions</a></h5>
-                       </center></div>
+                       </center></div></div>
+
+
                         </center></div></br></html>
