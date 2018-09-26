cab96ebd306a23ec9416434a94a73f7bfa472568
Diveyez <nemofbaltimore@gmail.com>
2018-09-25 21:18:19 -0700

    .

--- search.php
+++ search.php
@@ -67,7 +67,7 @@
                 curl_setopt($ch, CURLOPT_HEADER, false);                                                                         // fl=content removed to avoid debug clutter              //  &group=true&group.field=content //
 								curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true); $re = curl_exec($ch); curl_close($ch); $arr;
 								eval("\$arr = " . $re . ";"); echo "<p><b>You searched for:</b><i>" . $query . "</i>"; echo "</br>";
-								echo "<b>Found:</b><i>" . $arr['response']['numFound'] . "</i> results\n and displaying max 50.</p>" ;
+								echo "<b>Found:</b><i>" . $arr['doclist']['numFound'] . "</i> results\n and displaying max 50.</p>" ;
 								echo "<ptext><b>Found Matching Text/Terms:</b></ptext></br>";
 						    foreach($arr['response']['grouped'] as $text) { echo "<div id='ww2'><pcontent><b>Found:</b></br><b>" . $text['id'] . "</b></br>" .$text['content'] . "</br>" . "\n</pcontent></div></br></br>"; }
                 foreach($arr['response']['grouped'] as $text) { echo "<div id='ww2'><pcontent><b>Found:</b></br><b>" . $text['id'] . "</b></br>" .$text['highlighting'] . "</br>" . "\n</pcontent></div></br></br>"; }  ?>
