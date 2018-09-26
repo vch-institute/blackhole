c3f04879959afa5150ce968585e97532d343292a
Diveyez <nemofbaltimore@gmail.com>
2018-09-25 19:02:05 -0700

    .

--- queries.php
+++ queries.php
@@ -10,6 +10,35 @@
       </head>
 <!-- ADD SECURITY HEADERS ASAP FOR PUBLIC LIBRARY -->
 
+                <!-- RESPONSIVE NAV BAR --> <center>
+                <div class="topnav" id="myTopnav">
+                  <a href="index.php" class="active">Home</a>
+                  <a href="about.php">About</a>
+                  <a href="https://github.com/diveyez/blackhole">Contact Developer</a>
+                  <div class="dropdown">
+                    <button class="dropbtn">Explore
+                      <i class="fa fa-caret-down"></i>
+                    </button>
+                    <div class="dropdown-content">
+                      <a href="news.php">News</a>
+                      <a href="queries.php">Queries Explained</a>
+                      <a href="search.php">Search</a>
+                    </div>
+                  </div>
+                  <a href="#"></a>
+                  <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
+                </div>
+                <script>
+                function myFunction() {
+                    var x = document.getElementById("myTopnav");
+                    if (x.className === "topnav") {
+                        x.className += " responsive";
+                    } else {
+                        x.className = "topnav";
+                    }
+                }
+                </script></center>
+                <!--RESPONSIVE NAV BAR -->
 <!-- LEFT -->
 <body><div id="boxleft">
   <div class="row">
@@ -40,35 +69,6 @@
 
               <div id="cont">
 
-                <!-- RESPONSIVE NAV BAR --> <center>
-                <div class="topnav" id="myTopnav">
-                  <a href="index.php" class="active">Home</a>
-                  <a href="about.php">About</a>
-                  <a href="https://github.com/diveyez/blackhole">Contact Developer</a>
-                  <div class="dropdown">
-                    <button class="dropbtn">Explore
-                      <i class="fa fa-caret-down"></i>
-                    </button>
-                    <div class="dropdown-content">
-                      <a href="news.php">News</a>
-                      <a href="queries.php">Queries Explained</a>
-                      <a href="search.php">Search</a>
-                    </div>
-                  </div>
-                  <a href="#"></a>
-                  <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
-                </div>
-                <script>
-                function myFunction() {
-                    var x = document.getElementById("myTopnav");
-                    if (x.className === "topnav") {
-                        x.className += " responsive";
-                    } else {
-                        x.className = "topnav";
-                    }
-                }
-                </script></center>
-                <!--RESPONSIVE NAV BAR -->
 
 <body><center><a href="./"><img src="images/blackhole.png" height="125" width="190"></a></br>
                 <div class="tooltip">
