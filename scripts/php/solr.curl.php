9eee1f899051a9bfd256f499c3af73d9f15d5062
Diveyez <nemofbaltimore@gmail.com>
2018-09-26 01:12:40 -0700

    COMMITTING

---
+++ scripts/php/solr.curl.php
@@ -0,0 +1,15 @@
+<?php
+$ch = curl_init();
+$query = "";
+$encode = urlencode($query);
+curl_setopt($ch, CURLOPT_URL, "http://10.1.1.37:8983/solr/library/select?q=".$encode."&wt=php&indent=true&start=0q=god&wt=json&indent=true&fl=id,name,content&group=true&group.field=content&hl=on&hl.fl=*&hl.encoder=html&hl.fragmenter=regex&hl.regex.slop=1.0&hl.fl=text_*&hl.usePhraseHighlighter=true&hl.bs.type=WHOLE&hl.defaultSummary=true&hl.offsetSource=POSTINGS&hl.fragsize=125000&hl.maxAnalyzedChars=25000&hl.highlightMultiTerm=true&hl.tag.pre=<b>&hl.tag.post=</b>&results=50");
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
+$re = curl_exec($ch);
+curl_close($ch);
+$arr;
+eval("\$arr = " . $re . ";");
+echo "Found " . $arr['response']['numFound'] . " results\n";
+foreach($arr['response']['docs'] as $item) {
+    echo "title is : " . $item['name']. "\n"; }
+ ?>
--- scripts/php/solr.get.status.php
+++ scripts/php/solr.get.status.php
@@ -2,4 +2,5 @@
 $url = 'http://localhost:8983/solr/library/select?q=*:*';
 $contents = file_get_contents($url);
 $data = json_decode($contents, true);
+var_dump($data)
 ?>
