090e1375a718ed9153f3a6cd8e290868a4c3114e
Diveyez <nemofbaltimore@gmail.com>
2018-09-26 01:26:35 -0700

    .

--- scripts/php/solr.php.jsondecode.php
+++ scripts/php/solr.php.jsondecode.php
@@ -2,6 +2,6 @@
 $url = 'http://localhost:8983/solr/library/select?q=q=god&wt=json&indent=true&fl=id,name&group=true&group.field=content&hl=on&hl.fl=*&hl.encoder=html&hl.fragmenter=regex&hl.regex.slop=1.0&hl.fl=text_*&hl.usePhraseHighlighter=true&hl.bs.type=WHOLE&hl.defaultSummary=true&hl.offsetSource=POSTINGS&hl.fragsize=125000&hl.maxAnalyzedChars=25000&hl.highlightMultiTerm=true&hl.tag.pre=<b>&hl.tag.post=</b>&results=50';
 $contents = file_get_contents($url);
 $data = json_decode($contents, true);
-$docs = $data['response']['id']';
+$docs = $data['response']['id']['creator']['author']['highlighting'];
 var_dump($docs);
 ?>
