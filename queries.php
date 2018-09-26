7e80665229c00ebe295c3dcf8da9a051b5d56717
Diveyez <nemofbaltimore@gmail.com>
2018-09-25 19:07:01 -0700

    ..

--- queries.php
+++ queries.php
@@ -9,36 +9,6 @@
       <title>Black Hole Search - Notebooks, Libraries, AI, and Technology in one place.</title>
       </head>
 <!-- ADD SECURITY HEADERS ASAP FOR PUBLIC LIBRARY -->
-
-                <!-- RESPONSIVE NAV BAR --> <center>
-                <div class="topnav" id="myTopnav">
-                  <a href="index.php" class="active">Home</a>
-                  <a href="about.php">About</a>
-                  <a href="https://github.com/diveyez/blackhole">Contact Developer</a>
-                  <div class="dropdown">
-                    <button class="dropbtn">Explore
-                      <i class="fa fa-caret-down"></i>
-                    </button>
-                    <div class="dropdown-content">
-                      <a href="news.php">News</a>
-                      <a href="queries.php">Queries Explained</a>
-                      <a href="search.php">Search</a>
-                    </div>
-                  </div>
-                  <a href="#"></a>
-                  <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
-                </div>
-                <script>
-                function myFunction() {
-                    var x = document.getElementById("myTopnav");
-                    if (x.className === "topnav") {
-                        x.className += " responsive";
-                    } else {
-                        x.className = "topnav";
-                    }
-                }
-                </script></center>
-                <!--RESPONSIVE NAV BAR -->
 <!-- LEFT -->
 <body><div id="boxleft">
   <div class="row">
@@ -65,12 +35,38 @@
       <div class="column"></div>
       <div class="column"></div>
     </div></div>
-
-
-              <div id="cont">
-
-
-<body><center><a href="./"><img src="images/blackhole.png" height="125" width="190"></a></br>
+<!-- TOP CONTENT -->
+    	<body><div id="cont">
+        <!-- RESPONSIVE NAV BAR --> <center>
+        <div class="topnav" id="myTopnav">
+          <a href="index.php" class="active">Home</a>
+          <a href="about.php">About</a>
+          <a href="https://github.com/diveyez/blackhole">Contact Developer</a>
+          <div class="dropdown">
+            <button class="dropbtn">Explore
+              <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+              <a href="news.php">News</a>
+              <a href="queries.php">Queries Explained</a>
+              <a href="search.php">Search</a>
+            </div>
+          </div>
+          <a href="#"></a>
+          <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
+        </div>
+        <script>
+        function myFunction() {
+            var x = document.getElementById("myTopnav");
+            if (x.className === "topnav") {
+                x.className += " responsive";
+            } else {
+                x.className = "topnav";
+            }
+        }
+        </script></center>
+        <!--RESPONSIVE NAV BAR -->
+        <center><a href="./"><img src="images/blackhole.png" height="125" width="190"></a></br>
                 <div class="tooltip">
     						<span class="tooltiptext">Libraries:"This is whatever you called the core during setup. ie: 'library' "</b> </br> Server: localhost</span>
               <form method="post" action="search.php">
---
+++ template/skel-topcontent.php
@@ -0,0 +1,57 @@
+<!-- LEFT -->
+<body><div id="boxleft">
+  <div class="row">
+    <div class="column"></div>
+    <div class="column"></div>
+    <div class="column"></div>
+    <div class="column"></div>
+    <div class="column"></div>
+    <div class="column"></div>
+    <div class="column"></div>
+    <div class="column"></div>
+  </div></div>
+
+
+<!-- RIGHT -->
+  <div id="boxright">
+    <div class="row">
+      <div class="column"></div>
+      <div class="column"></div>
+      <div class="column"></div>
+      <div class="column"></div>
+      <div class="column"></div>
+      <div class="column"></div>
+      <div class="column"></div>
+      <div class="column"></div>
+    </div></div>
+<!-- TOP CONTENT -->
+    	<body><div id="cont">
+        <!-- RESPONSIVE NAV BAR --> <center>
+        <div class="topnav" id="myTopnav">
+          <a href="index.php" class="active">Home</a>
+          <a href="about.php">About</a>
+          <a href="https://github.com/diveyez/blackhole">Contact Developer</a>
+          <div class="dropdown">
+            <button class="dropbtn">Explore
+              <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+              <a href="news.php">News</a>
+              <a href="queries.php">Queries Explained</a>
+              <a href="search.php">Search</a>
+            </div>
+          </div>
+          <a href="#"></a>
+          <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
+        </div>
+        <script>
+        function myFunction() {
+            var x = document.getElementById("myTopnav");
+            if (x.className === "topnav") {
+                x.className += " responsive";
+            } else {
+                x.className = "topnav";
+            }
+        }
+        </script></center>
+        <!--RESPONSIVE NAV BAR -->
