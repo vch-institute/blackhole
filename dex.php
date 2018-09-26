4264f62e86ceea5cdff11146abc77cec3ac5c8be
Diveyez <nemofbaltimore@gmail.com>
2018-09-25 17:52:24 -0700

    ..

--- dex.php
+++ dex.php
@@ -4,16 +4,18 @@
   		<meta name="author" content="diveyez@r2nhosting.com"/>
   		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 			<link rel="stylesheet" type="text/css" href="style.css"/>
+      <link rel="stylesheet" type="text/css" href="nav.css"/>
+      <link rel="stylesheet" type="text/css" href="sidebar.css"/>
       <link href="http://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
       <title>Black Hole - Notebooks, Libraries, AI, and Technology for Logistics and Machine Learning</title>
       </head>
 <!-- ADD SECURITY HEADERS ASAP FOR PUBLIC LIBRARY -->
-<center><?php include("./template/skel-resp-nav.php"); ?></center>
+<center></center>
 <body>
-<?php include("./template/skel-header-image.php"); ?>
-<?php include("./template/skel-search.php"); ?>
-<?php include("./template/skel-about.php"); ?>
-<?php include("./template/skel-roadmap.php"); ?>
+  <div class="row">
+    <div class="column">TESTING</div>
+    <div class="column">TESTING</div>
+  </div>
 </body>
 <?php include("./template/skel-footer.php"); ?>
